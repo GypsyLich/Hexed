968f5a66d12e11a3ebe349e9fa80a1cab21f94f0
GypsyLich <eronin.m@mail.ru>
2019-05-19 19:25:26 +0700

    Add semi-working configuration for travisCI

--- Travis/build.sh
+++ Travis/build.sh
@@ -1,21 +1,21 @@
 #! /bin/sh
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -createManualActivationFile -logfile
+
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -manualLicenseFile Unity_v2018.x.ulf -logfile
+
 
 project="Hexed"
 
-echo "Attempting to build $project for Windows"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity 
-  -batchmode 
-  -nographics 
-  -silent-crashes 
-  -logFile $(pwd)/unity.log 
-  -projectPath $(pwd) 
-  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" 
+echo "Attempting to build $project for OS X"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile $(pwd)/unity.log \
+  -projectPath $(pwd) \
+  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
   -quit
 
 
 echo 'Logs from build'
 cat $(pwd)/unity.log
-
-
-echo 'Attempting to zip builds'
-zip -r $(pwd)/Build/windows.zip $(pwd)/Build/windows/
